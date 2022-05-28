fc6c58c88e8cbfbc15f76d18c0f9b5623d44c688
YTBJero <nguyenhieucoder47@gmail.com>
2022-05-28 20:16:33 +0700

    Added Copyright&Icon

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# AdvancedItem
-A plugin for PocketMine-MP that redeems and improves items.
+<h1>AdvancedItem<img src="assets/images/icon.png" height="64" width="64" align="left"></img></h1><br/>
+
+[![Lint](https://poggit.pmmp.io/ci.shield/BlockMagicDev/AdvancedItem/AdvancedItem)](https://poggit.pmmp.io/ci/BlockMagicDev/AdvancedItem/AdvancedItem)
+[![PHPStan](https://github.com/BlockMagicDev/AdvancedItem/actions/workflows/php.yml/badge.svg)](https://github.com/BlockMagicDev/AdvancedItem/actions/workflows/php.yml/badge.svg)
+
+✨ **A plugin for PocketMine-MP that redeems and improves items.**
--- src/BlockMagicDev/AdvancedItem/listeners/PlayerChat.php
+++ src/BlockMagicDev/AdvancedItem/listeners/PlayerChat.php
@@ -1,5 +1,26 @@
 <?php
 
+/*
+ *
+ *  ____  _            _    __  __             _
+ * |  _ \| |          | |  |  \/  |           (_)
+ * | |_) | | ___   ___| | _| \  / | __ _  __ _ _  ___
+ * |  _ <| |/ _ \ / __| |/ / |\/| |/ _` |/ _` | |/ __|
+ * | |_) | | (_) | (__|   <| |  | | (_| | (_| | | (__
+ * |____/|_|\___/ \___|_|\_\_|  |_|\__,_|\__, |_|\___|
+ *                                       __/ |
+ *                                      |___/
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author BlockMagicDev
+ * @link https://github.com/BlockMagicDev/Advanceditem
+ *
+ *
+*/
+
 declare(strict_types=1);
 
 namespace BlockMagicDev\AdvancedItem\listeners;
